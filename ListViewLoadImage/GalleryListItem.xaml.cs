cea27056cb6675bd0083e8bdbdbe75c02f873980
Breeze Liu (Shang Hai Wei Chuang Ruan Jian) <v-liuson@microsoft.com>
2017-05-29 18:07:04 +0800

    Add project files.

---
+++ ListViewLoadImage/GalleryListItem.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace ListViewLoadImage
+{
+    public sealed partial class GalleryListItem : UserControl
+    {
+        public GalleryListItem()
+        {
+            this.InitializeComponent();
+        }
+
+
+        /// <summary>
+        /// Create bindings for image path
+        /// </summary>
+        public string ImagePath
+        {
+            get
+            {
+                return (string)GetValue(imagePathProperty);
+            }
+            set
+            {
+                SetValue(imagePathProperty, value);
+            }
+        }
+
+        public static DependencyProperty imagePathProperty = DependencyProperty.Register("ImagePath", typeof(string), typeof(GalleryListItem), new PropertyMetadata("", ImagePathCallback));
+
+        public static async void ImagePathCallback(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            var imagePath = e.NewValue as string;
+            if (string.IsNullOrWhiteSpace(imagePath) || !File.Exists(imagePath)) return;
+
+            GalleryListItem gli = sender as GalleryListItem;
+
+            Uri uri = new Uri(imagePath);
+            // load the image
+            BitmapImage bitmap = new BitmapImage();
+           var imageFile = await StorageFile.GetFileFromApplicationUriAsync(uri);
+            using (var stream = await imageFile.OpenReadAsync())
+            {
+                await bitmap.SetSourceAsync(stream);
+            }
+
+            // set the image in the ListItem
+            await gli.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                gli.Image.Source = bitmap;
+            });
+        }
+    }
+}
---
+++ ListViewLoadImage/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ListViewLoadImage
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public ObservableCollection<Data> Images  { get;set;}
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Images = new ObservableCollection<Data>();
+            
+            Images.Add(new Data() { Path= @"ms-appx:///Assets/pic.png" });
+            Images.Add(new Data ());
+
+            this.DataContext = this;
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            StorageFile imageFile = await StorageFile.GetFileFromApplicationUriAsync(new Uri(@"ms-appx:///Assets/pic.png"));
+            Debug.WriteLine(imageFile.Name);
+        }
+    }
+
+    public class Data
+    {
+        public string Path { get; set; }
+    }
+}
